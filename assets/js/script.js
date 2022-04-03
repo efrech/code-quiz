3dbc548110314280239d8c7958d2e990d991d20f
Evelyn Frech <evelynmariafrech@gmail.com>
2022-04-02 20:31:01 -0400

    css & html code added

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//global selectors
+var  startBttn= document.querySelector("#start");
+
+//Store high scores
+//listener on start button, when click it starts the questions
+//timer starts and first question pops
+//play answers question and gets another question
+//score will be based on timer, is question is wrong, time is substracted from timer
+//game is over when all questions are answered or timer reaches 0
+//when game is over, user enters name and score 
